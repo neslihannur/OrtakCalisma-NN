0949db9fce2ab32a0596e95bfd31ca40a6ea37f6
neslihannur <hanneslihan34@gmail.com>
2024-09-08 18:02:19 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrtakCalisma_NN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
